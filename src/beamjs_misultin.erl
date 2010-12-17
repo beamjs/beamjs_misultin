3036eec3251a7ada27581a0fba6657573a5de066
Yurii Rashkovskii <yrashk@gmail.com>
2010-12-17 08:36:43 -0800

    Updated use of erlv8 API

--- src/beamjs_misultin.erl
+++ src/beamjs_misultin.erl
@@ -11,10 +11,10 @@
 		 }).
 
 exports() ->
-	[{"createServer", fun create_server/2}].
+	erlv8_object:new([{"createServer", fun create_server/2}]).
 
 create_server(#erlv8_fun_invocation{} = _Invocation, [Fun]) ->
-	[{"_callback", Fun},{"listen", fun listen/2}].
+	erlv8_object:new([{"_callback", Fun},{"listen", fun listen/2}]).
 
 listen(#erlv8_fun_invocation{ this = This } = _Invocation, [Port]) ->
 	case lists:keyfind(misultin,1,application:loaded_applications()) of
@@ -27,29 +27,37 @@ listen(#erlv8_fun_invocation{ this = This } = _Invocation, [Port]) ->
 				  {ok, _Pid} = misultin:start_link([{port, Port}, {loop, fun(Req) -> handle_http(This,Req) end}]),
 				  receive X -> X end
 		  end),
-	[{port, Port}].
+	erlv8_object:new([{port, Port}]).
 
 handle_http(This,Req) ->
-	F = proplists:get_value("_callback",This),
-	F:call([Req,resp_object(Req)]).
+	F = This:get_value("_callback"),
+	F:call([req_object(Req),resp_object(Req)]).
+
+req_object(Req) ->
+	erlv8_object:new([
+					  {"method",Req:get(method)},
+					  {"url",Req:get(uri)},
+					  {"headers",Req:get(headers)}
+					 ]).
 
 resp_object(Req) ->
-	[
-	 {"writeHead", fun (#erlv8_fun_invocation{ this = _This } = _Invocation, [Code, Headers]) ->
-						   Req:stream(head, Code, Headers),
-						   undefined
-				   end},
-	 {"write", fun (#erlv8_fun_invocation{ this = _This } = _Invocation, [String]) ->
-					 Req:stream(String),
-					 undefined
-				   end},
-	 {"end", fun (#erlv8_fun_invocation{ this = _This } = _Invocation, [String]) ->
-					 Req:stream(String),
-					 Req:stream(close),
-					 undefined;
-				 (#erlv8_fun_invocation{ this = _This } = _Invocation, []) ->
-					 Req:stream(close),
-					 undefined
-				   end}
-	 ].
+	erlv8_object:new(
+	  [
+	   {"writeHead", fun (#erlv8_fun_invocation{ this = _This } = _Invocation, [Code, Headers]) ->
+							 Req:stream(head, Code, Headers:proplist()),
+							 undefined
+					 end},
+	   {"write", fun (#erlv8_fun_invocation{ this = _This } = _Invocation, [String]) ->
+						 Req:stream(String),
+						 undefined
+				 end},
+	   {"end", fun (#erlv8_fun_invocation{ this = _This } = _Invocation, [String]) ->
+					   Req:stream(String),
+					   Req:stream(close),
+					   undefined;
+				   (#erlv8_fun_invocation{ this = _This } = _Invocation, []) ->
+					   Req:stream(close),
+					   undefined
+			   end}
+	  ]).
 						   
